19ad87ba4cf671dfdfc3101b675bc802bb69f436
Arjya Sen <114721138+virus3-0@users.noreply.github.com>
2023-06-08 11:50:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ let subjects = [];
 // Create a new subject
 app.post('/subjects', (req, res) => {
   const { id, name, isGraded, assignedClasses } = req.body;
+
+  const existingSubject = subjects.find(s => s.id === id || s.name === name);
+  if (existingSubject) {
+    return res.status(409).json({ error: 'Subject with the same ID or name already exists' });
+  }
+
   const newSubject = { id, name, isGraded, assignedClasses };
   subjects.push(newSubject);
   res.status(201).json(newSubject);
 });
+
 
 // Get all subjects
 app.get('/subjects', (req, res) => {
@@ -37,6 +44,29 @@ app.put('/subjects/:id', (req, res) => {
   res.json(subject);
 });
 
+// Update specific fields of a subject
+app.patch('/subjects/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, isGraded, assignedClasses } = req.body;
+
+  const subject = subjects.find(s => s.id === parseInt(id));
+  if (!subject) {
+    return res.status(404).json({ error: 'Subject not found' });
+  }
+
+  if (name) {
+    subject.name = name;
+  }
+  if (isGraded !== undefined) {
+    subject.isGraded = isGraded;
+  }
+  if (assignedClasses) {
+    subject.assignedClasses = assignedClasses;
+  }
+
+  res.json(subject);
+});
+
 // Delete a subject
 app.delete('/subjects/:id', (req, res) => {
   const { id } = req.params;
@@ -47,7 +77,7 @@ app.delete('/subjects/:id', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
