3cf161ba777440dc610dc1cb1b667eac9ad55caa
Arjya Sen <114721138+virus3-0@users.noreply.github.com>
2023-06-16 14:55:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.delete('/subjects/:id', (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
